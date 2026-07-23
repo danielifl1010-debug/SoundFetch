2e3efb9ecde66fabbc6bfe5dde6c362f2e7fac16
danielifl1010-debug <danielifl1010@gmail.com>
2026-07-23 22:20:09 +0000

    Fix output filename for reliable artifact upload

--- download.py
+++ download.py
@@ -1,5 +1,6 @@
 import yt_dlp
 import os
+import glob
 
 def download_song():
     song_name = os.environ.get("SONG_NAME", "יונתן שינפלד דרך חדשה")
@@ -13,7 +14,7 @@ def download_song():
             'preferredcodec': 'mp3',
             'preferredquality': '192',
         }],
-        'outtmpl': '%(title)s.%(ext)s',
+        'outtmpl': 'output_song.%(ext)s',
         'extractor_args': {'youtube': {'player_client': ['android', 'web']}}
     }
     
@@ -21,6 +22,10 @@ def download_song():
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([song_name])
         print("ההורדה הסתיימה בהצלחה!")
+        
+        # הדפסת כל הקבצים בתיקייה ליתר בטחון
+        print("קבצים בתיקייה:", glob.glob('*'))
+        
     except Exception as e:
         print(f"שגיאה בהורדה: {e}")
 

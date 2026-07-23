24a28224a0425ccc027d7acda990912daac6861b
danielifl1010-debug <danielifl1010@gmail.com>
2026-07-23 22:23:33 +0000

    Fix script to save as song.mp3 and make artifact optional

--- download.py
+++ download.py
@@ -1,6 +1,5 @@
 import yt_dlp
 import os
-import glob
 
 def download_song():
     song_name = os.environ.get("SONG_NAME", "יונתן שינפלד דרך חדשה")
@@ -14,7 +13,7 @@ def download_song():
             'preferredcodec': 'mp3',
             'preferredquality': '192',
         }],
-        'outtmpl': 'output_song.%(ext)s',
+        'outtmpl': 'song.%(ext)s',
         'extractor_args': {'youtube': {'player_client': ['android', 'web']}}
     }
     
@@ -22,10 +21,6 @@ def download_song():
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([song_name])
         print("ההורדה הסתיימה בהצלחה!")
-        
-        # הדפסת כל הקבצים בתיקייה ליתר בטחון
-        print("קבצים בתיקייה:", glob.glob('*'))
-        
     except Exception as e:
         print(f"שגיאה בהורדה: {e}")
 
